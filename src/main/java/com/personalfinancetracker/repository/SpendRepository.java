12208071fa954b336fcf9352272dfe3eefa482cd
arhdez87 <arhdez87@gmail.com>
2025-02-02 00:01:10 -0600

    I change spends to spend

--- src/main/java/com/personalfinancetracker/repository/SpendRepository.java
+++ src/main/java/com/personalfinancetracker/repository/SpendRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface SpendsRepository extends CrudRepository<Spend, UUID>, PagingAndSortingRepository<Spend, UUID> {
+public interface SpendRepository extends CrudRepository<Spend, UUID>, PagingAndSortingRepository<Spend, UUID> {
 }
